e668b336627da8144c157f0651bbde3856d698d8
aadeljoma <aadeljoma@gmail.com>
2019-05-25 22:11:22 +0300

    1.0

---
+++ project-6/truffle.js
@@ -0,0 +1,24 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const infuraKey = "b9c3cea567f647a88f89a5cabc4ad369";
+//
+// const fs = require('fs');
+const mnemonic = "dash inch blush nerve occur height marriage vast priority near cream parrot";
+
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
+      network_id: 4,       // rinkeby's id
+      gas: 4500000,        // rinkeby has a lower block limit than mainnet
+      gasPrice: 10000000000
+      // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
+  }
+};
